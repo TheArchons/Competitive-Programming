916319d1059c72f8468d6bd01512aa5f49b4e7e1
progamer9138 <39977408+progamer9138@users.noreply.github.com>
2021-12-17 20:27:33 -0800

    Redid J2-J4 2019, and implemented a broken version of J5
    J5 currently doesn't output the wanted value

---
+++ Junior/2019/J5.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <bits/stdc++.h>
+#include <utility>
+#include <tuple>
+#include <stack>
+using namespace std;
+
+pair<unordered_map<string, pair<string, int>>, unordered_set<int>> takeIns() { //takes inputs for substitutions
+    string temp;
+    unordered_map<string, pair<string, int>> replaceable;
+    unordered_set<int> lengths;
+    for (int i = 0; i < 3; i++) {
+        getline(cin, temp);
+        for (int j = 0; j < temp.length(); j++) {
+            if (temp[j] == ' ') {
+                replaceable[temp.substr(0, j)] = make_pair(temp.substr(j+1, temp.length()), i + 1);
+                lengths.insert(j + 1);
+            }
+        }
+    }
+    return make_pair(replaceable, lengths);
+}
+
+tuple<int, string, string> takeIn2() { // takes inputs for steps, start, and end
+    string temp;
+    getline(cin, temp);
+    int x;
+    string y, z;
+    bool didntFindSpace = true;
+    for (int i = 0; i < temp.length(); i++) {
+        int j;
+        if (didntFindSpace) {
+            if (temp[i] == ' ') {
+                didntFindSpace = false;
+                x = stoi(temp.substr(0, i));
+                j = i + 1;
+            }
+        } else {
+            if (temp[i] == ' ') {
+                y = temp.substr(j, i - j);
+                z = temp.substr(i + 1, temp.length() - i - 1);
+                break;
+            }
+        }
+    }
+    return (make_tuple(x, y, z));
+}
+
+tuple<int, int, string> findReplaceable(string currString, unordered_map<string, pair<string, int>> replaceable, unordered_set<int> lengths, vector<string> cantReplace) { //Possibility of this not functioning
+    while(!empty(lengths)) {
+        int length = *lengths.begin();
+        lengths.erase(lengths.begin()); //Ensure this doesn't change the original set
+        for (int i = 0; i < currString.length() - length + 1; i++) {
+            bool breakOut = false;
+            if (!replaceable.count(currString.substr(i, length - 1))) {
+                while (!empty(cantReplace)) {
+                    if (cantReplace.back() == currString.substr(i, length - 1)) {
+                        breakOut = true;
+                        break;
+                    }
+                    cantReplace.pop_back();
+                }
+                if (!breakOut) {
+                    return(make_tuple(replaceable.at(currString.substr(i, length)).second, i + 1, currString.substr(i, length)));
+                }
+            }
+        }
+    }
+    return make_tuple(0, 0, "");
+}
+
+vector<string> DFSSubstitutions(unordered_map<string, pair<string, int>> replaceable, unordered_set<int>, int steps, string startStr, string endStr, unordered_set<int> lengths) { //performs DFS
+    vector<string> visited, cannotReplace[50];
+    stack<string> prevStrings;
+    string current = startStr;
+    int stepCount = 0;
+    tuple<int, int, string, string> tempTuple;
+    while(current == endStr && stepCount == steps) {
+        tuple<int, int, string> tempTuple = findReplaceable(current, replaceable, lengths, cannotReplace[stepCount]);
+        if (get<0>(tempTuple) != 0) {
+            visited.push_back(to_string(get<0>(tempTuple)) + to_string(get<1>(tempTuple)) + get<2>(tempTuple));
+            current.erase(get<1>(tempTuple), get<2>(tempTuple).length());
+            current.insert(get<1>(tempTuple), replaceable.at(get<2>(tempTuple)).first);
+            cannotReplace[stepCount].push_back(current);
+            prevStrings.push(current);
+            continue;
+        }
+        //Reverses back by one
+        stepCount--;
+        current = prevStrings.top();
+        prevStrings.pop();
+    }
+    return visited;
+}
+
+void printVisited(vector<string> visited) {
+    cout << visited.size();
+    for (int i = 0; i < visited.size(); i++) {
+        cout << visited.at(i) << endl;
+    }
+}
+
+int main() {
+    string temp, startStr, endStr;
+    int steps;
+    pair<unordered_map<string, pair<string, int>>, unordered_set<int>> ins = takeIns(); //take inputs
+    unordered_map<string, pair<string, int>> replaceable = ins.first; unordered_set<int> lengths = ins.second; //allocates pair into respective variables
+    tuple<int, string, string> ins2 = takeIn2(); //take inputs
+    steps = get<0>(ins2); startStr = get<1>(ins2); endStr = get<2>(ins2); //allocates tuple into respective variables
+    vector<string> DFSSubs = DFSSubstitutions(replaceable, lengths, steps, startStr, endStr, lengths); //performs DFS
+    printVisited(DFSSubs); //print DFS
+    return 0;
+}
