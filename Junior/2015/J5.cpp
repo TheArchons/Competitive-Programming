f044d34bb1dcdb5e4df9e1100909d81f0b769239
progamer9138 <39977408+progamer9138@users.noreply.github.com>
2022-01-25 21:52:00 -0800

    J5 2015 Partial

--- Junior/2015/J5.cpp
+++ Junior/2015/J5.cpp
@@ -14,33 +14,18 @@ int main() {
     pies[peopleCount-1] += pieCount-peopleCount;
     //distribute the pies
     int distCount = 1;
-    bool changed = true;
-    while (changed) { //figure out loop stopping
-        next:
-        changed = false;
-        for (int i = peopleCount-1; i > 0; i--) {
-            for (int j = 0; j < peopleCount && pies[i-1] < pies[i]; j++) {
-                if (i <= j) { //skip if we try to distribute up or to ourselves
-                    continue;
-                }
-                //check for sharing validity
-                bool invalid = false;
-                for (int z = j; z <= i; z++) {
-                    if (z+1 >= sizeof(pies)/sizeof(pies[0])) continue;
-                    if (pies[z] + 1 > pies[z+1] || pies[z]+1 > pies[i]-1) {
-                        //cout << "invalid" << endl;
-                        invalid = true;
-                        break;
-                    }
-                }
-                if (invalid) {
-                    continue;
-                } 
-                pies[j]++;
-                pies[i]--;
+    next:
+    for (int i = peopleCount-1; i > 0; i--) {
+        for (int j = i-1; j >= 0; j--) {
+            pies[i]--;
+            pies[j]++;
+            if (pies[i] >= pies[i-1] && pies[j] <= pies[j+1]) {
                 distCount++;
-                changed = true;
                 goto next;
+            }
+            else{
+                pies[i]++;
+                pies[j]--;
             }
         }
     }
