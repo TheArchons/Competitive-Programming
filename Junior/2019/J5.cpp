e375b2b3fad3a054ca73048ceaa51cacbe213e72
progamer9138 <39977408+progamer9138@users.noreply.github.com>
2022-02-02 21:33:08 -0800

    J5 partial

--- Junior/2019/J5.cpp
+++ Junior/2019/J5.cpp
@@ -6,9 +6,9 @@
 
 using namespace std;
 
-bool isVisited(vector<pair<pair<int, int>, string>> visited, pair<pair<int, int>, string> v) {
-    for (pair<pair<int, int>, string> vv : visited) {
-        if (vv.first.first == v.first.first && vv.first.second == v.first.second && vv.second == v.second) {
+bool isVisited(vector<pair<int, string>> visited, pair<int, string> v) {\
+    for (auto i : visited) {
+        if (i.first == v.first && i.second == v.second) {
             return true;
         }
     }
@@ -34,7 +34,7 @@ int main() {
 
     stack<pair<pair<int, int>, string>> s; //for printing
     stack<string> s2; //to remember past strings
-    vector<pair<pair<int, int>, string>> visited;
+    vector<pair<int, string>> visited;
     string curr = start;
     bool found = false;
     next: //position uncertain
@@ -47,17 +47,17 @@ int main() {
                 for (int j = 0; j <= curr.length()-i; j++) {
                     string tempStr = curr.substr(j, i);
                     //check if it's a sub
-                    for (int k = 0; k <= subLens.size(); k++) {
+                    for (int k = 0; k < 3; k++) {
                         string find = subs[k].first;
                         string replace = subs[k].second;
                         if (tempStr == find) { //if the substr is a valid sub
-                            if (!isVisited(visited, make_pair(make_pair(k+1, j), curr))) { //if it hasn't been visited
-                                visited.push_back(make_pair(make_pair(k+1, j), curr));
+                            if (!isVisited(visited, make_pair(steps, curr))) { //if it hasn't been visited
                                 curr.replace(j, i, replace);
                                 s.push({{k+1, j}, curr});
                                 s2.push(curr);
                                 steps--;
                                 found = true;
+                                
                                 goto next;
                             }
                         }
@@ -67,11 +67,18 @@ int main() {
             }
         }
         else { //if we have reached the stepcount, backtrack
+            
             s2.pop();
-            curr = s2.top();
+            if (s2.empty()) {
+                curr = start;
+            }
+            else {
+                curr = s2.top();
+            }
             s.pop();
             steps++;
             found = true;
+            visited.push_back(make_pair(steps, curr));
         }
     }
     //reverse stack
