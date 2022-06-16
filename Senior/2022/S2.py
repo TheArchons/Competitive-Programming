af4116866c6dc0b0ff9eb4551734deba9db71995
TheArchons <progamer9138@gmail.com>
2022-06-16 14:43:05 -0700

    S2 2022

--- Senior/2022/S2.py
+++ Senior/2022/S2.py
@@ -3,16 +3,18 @@ sameGroups = int(input())
 
 for i in range(sameGroups):
     pair = input().split()
-    sameGroup[pair[0]] = pair[1]
-    sameGroup[pair[1]] = pair[0]
+    if pair[0] not in sameGroup:
+        sameGroup[pair[0]] = set()
+    sameGroup[pair[0]].add(pair[1])
 
 diffGroup = {}
 diffGroups = int(input())
 
 for i in range(diffGroups):
     pair = input().split()
-    diffGroup[pair[0]] = pair[1]
-    diffGroup[pair[1]] = pair[0]
+    if pair[0] not in diffGroup:
+        diffGroup[pair[0]] = set()
+    diffGroup[pair[0]].add(pair[1])
 
 violations = 0
 groups = int(input())
@@ -20,12 +22,12 @@ for group in range(groups):
     groupMembers = input().split()
     for i in range(3):
         if groupMembers[i] in sameGroup:
-            if not sameGroup[groupMembers[i]] in groupMembers:
-                sameGroup.pop(sameGroup[groupMembers[i]])
-                violations += 1
+            for member in sameGroup[groupMembers[i]]:
+                if not member in groupMembers:
+                    violations += 1
         if groupMembers[i] in diffGroup:
-            if diffGroup[groupMembers[i]] in groupMembers:
-                diffGroup.pop(diffGroup[groupMembers[i]])
-                violations += 1
+            for member in diffGroup[groupMembers[i]]:
+                if member in groupMembers:
+                    violations += 1
 
 print(violations)
