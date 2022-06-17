fda36c2e152215f1261ce9df41f92c47193842c7
TheArchons <progamer9138@gmail.com>
2022-06-16 22:42:50 -0700

    S3 partial

---
+++ Senior/2022/S3.py
@@ -0,0 +1,44 @@
+def goodSampleCount(notes, requiredCount):
+    count = 0
+    for t in range(len(notes)):
+        h = t
+        seen = set()
+        for i in range(len(notes) - t):
+            if notes[i] in seen:
+                break
+            count += 1
+            seen.add(notes[i])
+    if count == requiredCount:
+        return True
+    else:
+        return False
+
+temp = input().split()
+temp = [int(x) for x in temp]
+
+notes = temp[0]
+maxPitch = temp[1]
+goodSamples = temp[2]
+
+notePosses = []
+queue = [[1], [2]]
+while len(queue[0]) <= notes:
+    front = queue.pop(0)
+    queue.append(front + [1])
+    queue.append(front + [2])
+    if len(front) == notes:
+        notePosses.append(front)
+
+didBreak = False
+for i in notePosses:
+    if goodSampleCount(i, goodSamples):
+        for j in range(notes):
+            if j == notes-1:
+                print(i[j])
+            else:
+                print(i[j], end=' ')
+        didBreak = True
+        break
+
+if not didBreak:
+    print(-1)
