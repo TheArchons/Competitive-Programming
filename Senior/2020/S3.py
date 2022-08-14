e39f24c4bb1b65c576963449689e8d6b991e35e2
TheArchons <progamer9138@gmail.com>
2022-08-14 14:14:20 -0700

    S3 partial

--- Senior/2020/S3.py
+++ Senior/2020/S3.py
@@ -1,29 +1,47 @@
 needle = input()
 haystack = input()
 
-# find counts of each letter in the needle
-needle_counts = {}
-for letter in needle:
-    if letter in needle_counts:
-        needle_counts[letter] += 1
-    else:
-        needle_counts[letter] = 1
-
-results = set()
-
-# sliding window
-for i in range(len(haystack) - len(needle) + 1):
-    # check if the counts are the same
-    counts = {}
-    for j in range(i, i + len(needle)):
-        letter = haystack[j]
-        if letter in counts:
-            counts[letter] += 1
+# if the length of needle is longer than haystack, print 0
+if len(needle) > len(haystack):
+    print(0)
+else:
+    # find counts of each letter in the needle
+    needle_counts = {}
+    for letter in needle:
+        if letter in needle_counts:
+            needle_counts[letter] += 1
         else:
-            if letter not in needle_counts:
-                break
-            counts[letter] = 1
-    if counts == needle_counts:
-        results.add(haystack[i:i + len(needle)])
+            needle_counts[letter] = 1
 
-print(len(results))
+    results = set()
+    counts = {}
+
+    for i in range(len(needle)):
+        if haystack[i] in counts:
+            counts[haystack[i]] += 1
+        else:
+            counts[haystack[i]] = 1
+
+    if counts == needle_counts:
+        results.add(haystack[0:len(needle)])
+
+    counts[haystack[0]] -= 1
+    if counts[haystack[0]] == 0:
+        del counts[haystack[0]]
+
+    #count = 0
+
+    # sliding window
+    for i in range(1, len(haystack) - len(needle) + 1):
+        if haystack[i+len(needle)-1] in counts:
+            counts[haystack[i+len(needle)-1]] += 1
+        else:
+            counts[haystack[i+len(needle)-1]] = 1
+        if counts == needle_counts:
+            #count += 1
+            results.add(haystack[i:i + len(needle)])
+        counts[haystack[i]] -= 1
+        if counts[haystack[i]] == 0:
+            del counts[haystack[i]]
+
+    print(len(results))
