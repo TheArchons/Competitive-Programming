449a57bbe6e7b1c8de74154f3712095d1f8de9db
TheArchons <progamer9138@gmail.com>
2022-08-17 22:05:37 -0700

    2020 S3 Partial

--- Senior/2020/S3.py
+++ Senior/2020/S3.py
@@ -1,3 +1,7 @@
+def createCounts():
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    return {letter: 0 for letter in letters}
+
 needle = input()
 haystack = input()
 
@@ -6,48 +10,28 @@ if len(needle) > len(haystack):
     print(0)
 else:
     # find counts of each letter in the needle
-    needle_counts = {}
+    needle_counts = createCounts()
     for letter in needle:
-        if letter in needle_counts:
-            needle_counts[letter] += 1
-        else:
-            needle_counts[letter] = 1
+        needle_counts[letter] += 1
 
-    results = []
-    counts = {}
+    results = set()
+    counts = createCounts()
 
     for i in range(len(needle)):
-        if haystack[i] in counts:
-            counts[haystack[i]] += 1
-        else:
-            counts[haystack[i]] = 1
+        counts[haystack[i]] += 1
 
     if counts == needle_counts:
-        results.append(0)
+        results.add(hash(haystack[0:len(needle)]))
 
     counts[haystack[0]] -= 1
-    if counts[haystack[0]] == 0:
-        del counts[haystack[0]]
 
-    #count = 0
+    count = 0
 
     # sliding window
     for i in range(1, len(haystack) - len(needle) + 1):
-        if haystack[i+len(needle)-1] in counts:
-            counts[haystack[i+len(needle)-1]] += 1
-        else:
-            counts[haystack[i+len(needle)-1]] = 1
+        counts[haystack[i+len(needle)-1]] += 1
         if counts == needle_counts:
-            #count += 1
-            results.append(i)
+            results.add(hash(haystack[i:i+len(needle)]))
         counts[haystack[i]] -= 1
-        if counts[haystack[i]] == 0:
-            del counts[haystack[i]]
 
-    # remove duplicates
-    removes = set()
-    for i in range(len(results)):
-        for j in range(i+1, len(results)):
-            if haystack[results[i]:results[i]+len(needle)] == haystack[results[j]:results[j]+len(needle)]:
-                removes.add(j)
-    print(len(results) - len(removes))
+    print(len(results))
