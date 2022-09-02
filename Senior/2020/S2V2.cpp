833a716d4f9160e29a5d386503fce4da334aeb4a
TheArchons <progamer9138@gmail.com>
2022-09-02 11:37:37 -0700

    S2 + S3 partial

---
+++ Senior/2020/S2V2.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <map>
+#include <vector>
+#include <tuple>
+#include <set>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    int rows, cols;
+    scanf("%d\n%d", &rows, &cols);
+
+    map<int, vector<tuple<int, int>>> poses;
+
+    int room[rows][cols] = {0};
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            int val;
+            scanf("%d", &val);
+            room[i][j] = val;
+            poses[val].push_back(make_tuple(i, j));
+        }
+    }
+
+    tuple<int, int> curr = make_tuple(rows-1, cols-1);
+
+    set<tuple<int, int>> visited;
+
+    stack<tuple<int, int>> path;
+
+    while (curr != make_tuple(0, 0)) {
+        int val = room[get<0>(curr)][get<1>(curr)];
+        int find = get<0>(curr) + 1 * get<1>(curr) + 1;
+        visited.insert(curr);
+        path.push(curr);
+
+        bool found = false;
+        for (int i = 0; i < poses[find].size(); i++) {
+            // if not visited
+            if (visited.find(poses[find][i]) == visited.end()) {
+                curr = poses[find][i];
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            poses[val].pop_back();
+        } else {
+            path.pop();
+            if (path.empty()) {
+                printf("no");
+                return 0;
+            }
+            curr = path.top();
+        }
+
+
+
+        
+
+    }
+
+    printf("yes");
+
+    return 0;
+}
--- Senior/2021/s3.cpp
+++ Senior/2021/s3.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <climits>
 #include <stdlib.h>
+#include <algorithm>
 
 using namespace std;
 
 #define inf INT_MAX
+
+bool compare(int a[], int b[]) {
+    return a[0] < b[0];
+}
 
 // N - number of friends (0 for me)
 // P - Position
@@ -14,30 +19,16 @@ using namespace std;
 int main() {
     unsigned int n;
     scanf("%d", &n);
-    int arr[n][3];
+    int arr[n][3] = {{0}};
     for (int i = 0; i < n; i++) {
         scanf("%d %d %d", &arr[i][0], &arr[i][1], &arr[i][2]);
     }
 
     unsigned int min_dist = inf;
-    for (int i = 0; i < n; i++) {
-        int loc1 = arr[i][0]-arr[i][2];
-        int loc2 = arr[i][0]+arr[i][2];
-        int locations[2] = {loc1, loc2};
-        for (int k = 0; k < 2; k++) {
-            unsigned int dist = 0;
-            for (int j = 0; j < n; j++) {
-                int walkTime = (abs(locations[k] - arr[j][0])-arr[j][2])*arr[j][1];
-                if (walkTime < 0) {
-                    walkTime = 0;
-                }
-                dist += walkTime;
-            }
-            if (dist < min_dist) {
-                min_dist = dist;
-            }
-        }
-    }
+
+    // sort by arr[i][0]
+    sort(arr, arr + n, compare);
+
 
     printf("%d\n", min_dist);
 }
