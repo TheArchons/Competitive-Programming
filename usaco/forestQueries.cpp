8bb3a7fc0ce3f8b74a8a45d44f4ccc8a09be941b
TheArchons <progamer9138@gmail.com>
2022-09-30 15:33:10 -0700

    USACO Forest Queries constant time

--- usaco/forestQueries.cpp
+++ usaco/forestQueries.cpp
@@ -15,20 +15,30 @@ int main() {
         }
     }
 
+    // prefix top down
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            a[i][j] += (i == 0 ? 0 : a[i-1][j]);
+        }
+    }
+
     for (int i = 0; i < q; i++) {
         int y1, x1, y2, x2;
         scanf("%d %d %d %d", &y1, &x1, &y2, &x2);
 
-        int sum = 0;
-        for (int j = y1-1; j < y2; j++) {
-            if (x1 == 1) {
-                sum += a[j][x2-1];
-            }
-            else {
-                sum += a[j][x2-1] - a[j][x1-2];
-            }
+        int sum = a[y2-1][x2-1];
+        if (y1 > 1) {
+            sum -= a[y1-2][x2-1];
         }
+        if (x1 > 1) {
+            sum -= a[y2-1][x1-2];
+        }
+        if (y1 > 1 && x1 > 1) {
+            sum += a[y1-2][x1-2];
+        }
+
         printf("%d\n", sum);
+
     }
 
     return 0;
