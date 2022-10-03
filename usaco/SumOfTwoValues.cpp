01a7b5cc69ff3876c41064237c15843bdab5db32
TheArchons <progamer9138@gmail.com>
2022-10-03 11:58:45 -0700

    SumOfTwoValues
    This is just 2sum

---
+++ usaco/SumOfTwoValues.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <map>
+#include <chrono>
+
+using namespace std;
+
+int main() {
+    //freopen("twosum.in", "r", stdin); freopen("twosum.out", "w", stdout);
+    int arrLen, target;
+    scanf("%d %d", &arrLen, &target);
+
+    int arr[arrLen];
+    map<int, int> map;
+    
+    for (int i = 0; i < arrLen; i++) {
+        scanf("%d", &arr[i]);
+        map[arr[i]] = i;
+    }
+
+    for (int i = 0; i < arrLen; i++) {
+        int diff = target - arr[i];
+        if (map.count(diff) && map[diff] != i) {
+            printf("%d %d", i + 1, map[diff] + 1);
+            return 0;
+        }
+    }
+
+    printf("IMPOSSIBLE");
+    return 0;
+}
