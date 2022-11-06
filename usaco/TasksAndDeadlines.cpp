d1464c885260629c0754e5189d9e4e360a2a0169
TheArchons <progamer9138@gmail.com>
2022-11-06 12:41:56 -0800

    Tasks and Deadlines partial

---
+++ usaco/TasksAndDeadlines.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <tuple>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    tuple<int, int> tasks[n];
+    for (int i = 0; i < n; i++) {
+        int duration, deadline;
+        scanf("%d %d", &duration, &deadline);
+
+        tasks[i] = make_tuple(duration, deadline);
+    }
+
+    sort(tasks, tasks + n);
+
+    int reward; int time;
+    reward = time = 0;
+    for (int i = 0; i < n; i++) {
+        int duration = get<0>(tasks[i]);
+        int deadline = get<1>(tasks[i]);
+
+        time += duration;
+
+        reward += deadline - time;
+    }
+
+    printf("%d", reward);
+}
