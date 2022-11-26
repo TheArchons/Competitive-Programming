0097369fcd5512cf1e0c785fd72b83b40ac22b3c
TheArchons <progamer9138@gmail.com>
2022-11-25 17:59:13 -0800

    2012 S3 partial

--- Waterloo CCC/Senior/2012/s3.cpp
+++ Waterloo CCC/Senior/2012/s3.cpp
@@ -1,54 +1,48 @@
 #include <stdio.h>
 #include <map>
 #include <math.h>
-#include <unordered_set>
+#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 int main() {
     int sensorNum;
     scanf("%d", &sensorNum);
-    
-    int max1, max2, min1, min2, freq1, freq2;
-    max1 = max2 = 2000001;
-    min1 = min2 = -2000001;
-    freq1 = freq2 = 0;
-    map<int, int> sensors;
-    unordered_set<int> maxVals;
 
-    for (int i = 0; i < sensorNum; i++) {
+    unordered_map<int, int> sensorMap;
+    
+    for(int i = 0; i < sensorNum; i++) {
         int sensor;
         scanf("%d", &sensor);
-        sensors[sensor]++;
-        if (sensors[sensor] > freq1) {
-            freq2 = freq1;
-            max2 = max1;
-            min2 = min1;
-            freq1 = sensors[sensor];
-            max1 = min1 = sensor;
-            maxVals.clear();
-            maxVals.insert(sensor);
-        } else if (sensors[sensor] == freq1) {
-            maxVals.insert(sensor);
-            if (sensor > max1) {
-                max1 = sensor;
-            } else if (sensor < min1) {
-                min1 = sensor;
-            }
-        } else if (sensors[sensor] > freq2) {
-            freq2 = sensors[sensor];
-            max2 = min2 = sensor;
-        } else if (sensors[sensor] == freq2) {
-            if (sensor > max2) {
-                max2 = sensor;
-            } else if (sensor < min2) {
-                min2 = sensor;
-            }
+        sensorMap[sensor]++;
+    }
+
+    int maxFreq, secondMaxFreq;
+    maxFreq = secondMaxFreq = 0;
+    for (auto it = sensorMap.begin(); it != sensorMap.end(); it++) {
+        if (it->second > maxFreq) {
+            secondMaxFreq = maxFreq;
+            maxFreq = it->second;
+        } else if (it->second > secondMaxFreq) {
+            secondMaxFreq = it->second;
         }
     }
-    if (max2 == 2000001) {
-        max2 = max1;
-        min2 = min1;
+
+    int maxFreqSensor, secondMaxFreqSensor, minFreqSensor, secondMinFreqSensor;
+    maxFreqSensor = secondMaxFreqSensor = -20000001;
+    minFreqSensor = secondMinFreqSensor = 20000001;
+
+    for (auto it = sensorMap.begin(); it != sensorMap.end(); it++) {
+        if (it->second == maxFreq) {
+            maxFreqSensor = max(maxFreqSensor, it->first);
+            minFreqSensor = min(minFreqSensor, it->first);
+        }
+        else if (it->second == secondMaxFreq) {
+            secondMaxFreqSensor = max(secondMaxFreqSensor, it->first);
+            secondMinFreqSensor = min(secondMinFreqSensor, it->first);
+        }
     }
-    printf("%d", max(abs(max1 - min2), abs(max2 - min1)));
+
+    printf("%d", max(abs(maxFreqSensor - secondMinFreqSensor), abs(secondMaxFreqSensor - minFreqSensor)));
 }
