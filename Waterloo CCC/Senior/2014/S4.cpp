404680c4e94de263420bb13deae60e0463853f96
TheArchons <progamer9138@gmail.com>
2022-12-02 19:52:31 -0800

    waterloo S4 partial

--- Waterloo CCC/Senior/2014/S4.cpp
+++ Waterloo CCC/Senior/2014/S4.cpp
@@ -1,38 +1,65 @@
-#include <iostream>
-#include <math.h>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 /*
 Store all the boxes in a vector
 Compare each box with the other boxes.
-Save a vector of points where at least one box is present, and one for each section where a box overlaps another
-
+Save a vector of points where at least one box is present, and one for each section where a box overlaps another (prevent duplicates by checking if the point is within another)
+For each point, if there are at least T overlapping boxes, calculate the area of the section by taking the minimum of the boxes' dimensions
 */
 
-/*struct box{
-    int lx, rx, by, ty;
-    int tint;
-    box(int lx, int rx, int by, int ty, int tint){
-        this->lx = lx;
-        this->rx = rx;
-        this->by = by;
-        this->ty = ty;
-        this->tint = tint;
-    }
-
-    bool isColliding(box &other) {
-        //check to see if 2 boxes are colliding
-        if ((this->lx < other.rx && this->ty > other.by) || (this->lx < other.rx && this->by < other.ty) || (this->rx > other.lx && this->ty > other.by) || (this->rx > other.lx && this->by < other.ty)) {
-            return true;
-        }
-        return false;
-    }
+struct box {
+    int x1, y1, x2, y2;
 };
 
+pair<int, int> compareTopLeft(box a, box b) {
+    if (a.x1 <= b.x2 && a.y1 <= b.y2) return make_pair(a.x1, a.y1); 
+    else return make_pair(-1, -1);
+}
+
+pair<int, int> compareTopRight(box a, box b) {
+    if (a.x2 >= b.x1 && a.y1 <= b.y2) return make_pair(a.x2, a.y1);
+    else return make_pair(-1, -1);
+}
+
+pair<int, int> compareBottomLeft(box a, box b) {
+    if (a.x1 <= b.x2 && a.y2 >= b.y1) return make_pair(a.x1, a.y2);
+    else return make_pair(-1, -1);
+}
+
+pair<int, int> compareBottomRight(box a, box b) {
+    if (a.x2 >= b.x1 && a.y2 >= b.y1) return make_pair(a.x2, a.y2);
+    else return make_pair(-1, -1);
+}
+
 int main() {
-    //box1 and 2
-    int box1lx, box1rx, box1by, box1ty, box2lx, box2rx, box2by, box2ty, tint;
-    
-    return 0;
-}*/
+    int n, t;
+    scanf("%d %d", &n, &t);
+
+    vector<box> boxes(n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d %d %d", &boxes[i].x1, &boxes[i].y1, &boxes[i].x2, &boxes[i].y2);
+    }
+
+    vector<pair<int, int>> points; // x, y
+    for (int i = 0; i < boxes.size(); i++) {
+        for (int j = i+1; j < boxes.size(); j++) {
+            // test for overlap
+            // DRY? What's that?
+            pair<int, int> topLeft = compareTopLeft(boxes[i], boxes[j]);
+            pair<int, int> topRight = compareTopRight(boxes[i], boxes[j]);
+            pair<int, int> bottomLeft = compareBottomLeft(boxes[i], boxes[j]);
+            pair<int, int> bottomRight = compareBottomRight(boxes[i], boxes[j]);
+
+            if (topLeft.first != -1) {
+                points.push_back(topLeft);
+            } else if (topRight.first != -1) {
+                points.push_back(topRight);
+            } else if (bottomLeft.first != -1) {
+                points.push_back(bottomLeft);
+            } else if (bottomRight.first != -1) {
+                points.push_back(bottomRight);
+            }
+        }
+    }
+}
