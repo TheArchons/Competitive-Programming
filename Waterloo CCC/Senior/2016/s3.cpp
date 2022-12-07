f5edbde7c45932aeca2a891a4695bdf9f48eccbd
TheArchons <progamer9138@gmail.com>
2022-12-06 21:19:17 -0800

    CCC 2016 S3 partial

--- Waterloo CCC/Senior/2016/s3.cpp
+++ Waterloo CCC/Senior/2016/s3.cpp
@@ -1,184 +1,141 @@
-#include <iostream>
-#include <stdio.h>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void printVec(vector<int> vec){
-    for(int i:vec){
-        cout << i << " ";
-    }
-    cout << endl;
-}
-void print2dVec(vector<vector<int>> vec){
-    for(vector<int> i : vec){
-        printVec(i);
-    }
-    cout << endl;
-}
+/*
+1. Remove all useless nodes (restaurants that do not lead to a pho restaurant) by doing a DFS from the pho restaurants
+2. Pick one pho restaurant that only has one connection as the root (because all useless nodes are removed, it is guaranteed that there is at least one such node)
+3. Calculate the depth of each node from the root
+    1. Do a DFS from the root
+    2. For each node in path, set the depth of the node for max(current depth of node, number of edges between the node and the first node of the path)
+4. DFS but choose the node with the lowest depth first.
+5. When all restaurants are visited, return the number of edges traveled
+*/
 
-void printQueue(queue<int> q){
-    while(!q.empty()){
-        cout << q.front() << " ";
-        q.pop();
-    }
-    cout << endl;
-}
+int main() {
+    //freopen("3.input", "r", stdin); // for testing
+    
+    int nodes, phoNum;
+    scanf("%d %d\n", &nodes, &phoNum);
 
-bool isShop(vector<int> shops, int i){ //IMPROVE IF TLE
-    //search for shop
-    for(int j = 0; j < shops.size(); j++){
-        if(shops[j] == i){
-            return true;
+    set<int> phos;
+    for (int i = 0; i < phoNum; i++) {
+        int temp;
+        scanf("%d", &temp);
+        phos.insert(temp);
+    }
+
+    map<int, vector<int>> connections;
+    for (int i = 0; i < nodes-1; i++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        connections[a].push_back(b);
+        connections[b].push_back(a);
+    }
+
+    // Remove all useless nodes
+    set<int> visited, tempVisited;
+    vector<int> path;
+    path.push_back(*phos.begin());
+    
+    while (!path.empty()) {
+        int current = path.back();
+        path.pop_back();
+        tempVisited.insert(current);
+
+        if (phos.find(current) == phos.end() && connections[current].size() == 1) {
+            visited.insert(current);
+        }
+
+        for (int i = 0; i < connections[current].size(); i++) {
+            if (tempVisited.find(connections[current][i]) == tempVisited.end()) {
+                path.push_back(connections[current][i]);
+            }
         }
     }
-    return false;
-}
 
-int main(){
-    int numLines,numShops;
-    vector<int> shopLocations;
-
-    scanf("%d %d\n",&numLines,&numShops);
-    for(int i=0;i<numShops;i++){
-        int shopIndex;
-        cin >> shopIndex;
-        shopLocations.push_back(shopIndex);
-    }
-    vector<vector<int>> tree(numLines);
-
-    for(int i=0;i<numLines-1;i++){
-        int src,dst;
-        cin >> src;
-        cin >> dst;
-        tree[src].push_back(dst);
-        tree[dst].push_back(src);
-    }
-    //pick a random spot to call our tail
-    //expand to find outself another node to call as our head
-    //  move the head and tail in all directions (breadth first)
-    //  if the head sees that the tail has already been there, dont
-    //      go down that path (as the tail will get there first), and vice versa
-    //  If either the head or tail sees a new node, assign that node to whichever
-    //      head or tail has reached that node (order doesnt matter)
-    //  Once we have found a new head/tail, clear the nodes, and start counting
-    //      up again
-
-    // print tree
-    //cout << endl;
-    //print2dVec(tree);
-    int head,tail;
-    tail = head = shopLocations[0];
-    shopLocations.erase(shopLocations.begin());
-    int steps = 0;
-    //bfs to find head
-
-    while(!shopLocations.empty()){
-        //cout << endl;
-        queue<vector<int>> hToVisit, tToVisit; //head and tail to visit. Vector to keep track of steps
-        hToVisit.push({head}); tToVisit.push({tail});
-        int curr = hToVisit.front().front();
-        vector<vector<int>> visitedTree = tree; //copy of tree to keep track of visited nodes
-        vector<int> toVisit;
-        bool foundShop = false;
-        //bool foundHShop, foundTShop;
-        //foundHShop = foundTShop = false;
-        while (true){ //while we have not found a shop
-            steps++;
-            while(!hToVisit.front().empty()) { //bfs one layer for head
-                curr = hToVisit.front().front();
-                hToVisit.front().erase(hToVisit.front().begin());
-                for(int i = 0; i < visitedTree[curr].size(); i++){
-                    if (visitedTree[curr][i] == -1 || visitedTree[curr][i] == tail || visitedTree[curr][i] == head){
-                        continue;
-                    }
-                    if(!isShop(shopLocations,visitedTree[curr][i])){
-                        toVisit.push_back(visitedTree[curr][i]); //add to queue and mark as visited
-                        //prevent going back
-                        for (int j = 0; j < visitedTree[visitedTree[curr][i]].size(); j++) {
-                            if (visitedTree[visitedTree[curr][i]][j] == curr) {
-                                visitedTree[visitedTree[curr][i]][j] = -1;
-                            }
-                        }
-                        //cout << "visited " << visitedTree[curr][i] << endl;
-                        visitedTree[curr][i] =  -1;
-                    }
-                    else{
-                        head = visitedTree[curr][i]; //found head, so assign it
-                        shopLocations.erase(find(shopLocations.begin(),shopLocations.end(),head));
-                        //foundHShop = true;
-                        foundShop = true;
-                        //steps++;
-                        break;
-                    }
-                }
-                if(foundShop){
-                    break;
-                }
-            }
-            if (foundShop) {
-                //cout << head << endl;
-                break;
-            }
-            hToVisit.pop();
-            if (!toVisit.empty()) {
-                hToVisit.push(toVisit);
-                toVisit.clear();
-            }
-            else {
-                hToVisit.push({head});
-            }
-            //I probably should've used a function
-            while(!tToVisit.front().empty()) { //bfs one layer for tail
-                curr = tToVisit.front().front();
-                tToVisit.front().erase(tToVisit.front().begin());
-                for(int i = 0; i < visitedTree[curr].size(); i++){
-                    if (visitedTree[curr][i] == -1 || visitedTree[curr][i] == tail || visitedTree[curr][i] == head) {
-                        continue;
-                    }
-                    if(!isShop(shopLocations,visitedTree[curr][i])){
-                        toVisit.push_back(visitedTree[curr][i]); //add to queue and mark as visited
-                        for (int j = 0; j < visitedTree[visitedTree[curr][i]].size(); j++) {
-                            if (visitedTree[visitedTree[curr][i]][j] == curr) {
-                                visitedTree[visitedTree[curr][i]][j] = -1;
-                            }
-                        }
-                        //cout << "visited " << visitedTree[curr][i] << endl;
-                        visitedTree[curr][i] = -1;
-                    }
-                    else{
-                        tail = visitedTree[curr][i]; //found tail, so assign it
-                        shopLocations.erase(find(shopLocations.begin(),shopLocations.end(),tail));
-                        //foundTShop = true;
-                        //steps++;
-                        foundShop = true;
-                        break;
-                    }
-                }
-                if (foundShop) {
-                    break;
-                }
-            }
-            if (foundShop) {
-                //cout << tail << endl;
-                break;
-            }
-            tToVisit.pop();
-            if (!toVisit.empty()) {
-                tToVisit.push(toVisit);
-                toVisit.clear();
-            }
-            else {
-                tToVisit.push({tail});
-            }
-            //pop tToVisit if not empty
-            
-            //cout << "steps: " << steps << endl;
+    // Find the root
+    int root;
+    for (auto it = phos.begin(); it != phos.end(); it++) {
+        if (connections[*it].size() == 1) {
+            root = *it;
+            break;
         }
     }
-    cout << steps;
+
+    // Calculate the depth of each node
+    int depths[nodes] = {0};
+
+    path.push_back(root);
+    tempVisited.clear();
+
+    int curr = root;
+    int visitedPhoNum = 1;
+
+    while (visitedPhoNum < phos.size()) {
+        bool found = false;
+        tempVisited.insert(curr);
+
+        // calculate the depth of all nodes in the path
+        for (int i = path.size() - 2; i >= 0; i--) {
+            depths[path[i]] = max(depths[path[i]], (int)path.size() - i - 1);
+        }
+
+        for (int i = 0; i < connections[curr].size(); i++) {
+            if (tempVisited.find(connections[curr][i]) == tempVisited.end() && visited.find(connections[curr][i]) == visited.end()) {
+                curr = connections[curr][i];
+                path.push_back(curr);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            path.pop_back();
+            curr = path.back();
+        }
+
+        if (phos.find(curr) != phos.end()) {
+            visitedPhoNum++;
+        }
+    }
+
+    // DFS but choose the node with the lowest depth first
+    path.clear();
+    path.push_back(root);
+    curr = root;
+    visitedPhoNum = 1;
+    int dist = 0;
+
+    while (visitedPhoNum < phos.size()) {
+        visited.insert(curr);
+
+        int minDepth = INT_MAX;
+        int minDepthNode = -1;
+
+        for (int i = 0; i < connections[curr].size(); i++) {
+            if (visited.find(connections[curr][i]) == visited.end()) {
+                if (depths[connections[curr][i]] < minDepth) {
+                    minDepth = depths[connections[curr][i]];
+                    minDepthNode = connections[curr][i];
+                }
+            }
+        }
+
+        if (minDepthNode != -1) {
+            curr = minDepthNode;
+            path.push_back(curr);
+            if (phos.find(curr) != phos.end()) visitedPhoNum++;
+        } else {
+            path.pop_back();
+            curr = path.back();
+        }
+
+        dist++;
+    }
+
+    printf("%d", dist);
+
     return 0;
 }
