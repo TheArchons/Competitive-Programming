b259fd11dcca5ca894cc7d3d2b44c04d81405870
TheArchons <progamer9138@gmail.com>
2022-12-11 13:41:05 -0800

    ccc 2018 s1 partial

--- CCC/Senior/2018/S1.cpp
+++ CCC/Senior/2018/S1.cpp
@@ -1,44 +1,30 @@
-#include <stdio.h>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    int len;
-    vector<int> villages;
-    scanf("%d", &len);
-    for (int i = 0; i < len; i++) {
-        int temp;
-        scanf("%d", &temp);
-        villages.push_back(temp);
+    //freopen("1.input", "r", stdin); // for testing
+    
+    int villageNum;
+    scanf("%d", &villageNum);
+
+    set<int> villages;
+    for (int i = 0; i < villageNum; i++) {
+        int village;
+        scanf("%d", &village);
+        villages.insert(village);
     }
-    //sort
-    sort(villages.begin(), villages.begin()+len);
-    int prev, prevTotal;
-    float prevArea, min;
-    prevTotal = 0;
-    min = prev = -1;
-    for (int i : villages) {
-        if (prev == -1) {
-            prev = i;
-            prevArea = 0;
-            continue;
-        }
-        float area = prevArea + ((i - prev)/2);
-        
-        if (prevArea == 0) {
-            prevArea = area-prevArea;
-            continue;
-        }
-        prevArea = area-prevArea;
-        if (min == -1 || (area-prevTotal < min && area-prevTotal > 0)) {
-            min = area-prevTotal;
-        }
-        prevTotal += area-prevTotal;
+
+    float minNeighbour = INT_MAX;
+    for (auto it = villages.begin(); it != villages.end(); it++) {
+        if (it == villages.begin() || it == --villages.end()) continue;
+        auto left = it;
+        auto right = it;
+        left--; right++;
+        float leftDist = (*it - *left) / 2.0;
+        float rightDist = (*right - *it) / 2.0;
+        minNeighbour = min(minNeighbour, (leftDist + rightDist));
     }
-    //1 decimal
-    printf("%.1f\n", min);
-    return 0;
+
+    printf("%.1f", minNeighbour);
 }
