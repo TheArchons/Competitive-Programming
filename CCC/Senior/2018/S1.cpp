ea7d8c0a4d7a6fe93bce25925e86f3a90d0e087e
TheArchons <progamer9138@gmail.com>
2022-12-11 17:24:45 -0800

    CCC 2018 S1

--- CCC/Senior/2018/S1.cpp
+++ CCC/Senior/2018/S1.cpp
@@ -15,14 +15,14 @@ int main() {
         villages.insert(village);
     }
 
-    float minNeighbour = INT_MAX;
+    double minNeighbour = INT_MAX;
     for (auto it = villages.begin(); it != villages.end(); it++) {
         if (it == villages.begin() || it == --villages.end()) continue;
         auto left = it;
         auto right = it;
         left--; right++;
-        float leftDist = (*it - *left) / 2.0;
-        float rightDist = (*right - *it) / 2.0;
+        double leftDist = abs(*it - *left) / 2.0;
+        double rightDist = abs(*right - *it) / 2.0;
         minNeighbour = min(minNeighbour, (leftDist + rightDist));
     }
 
