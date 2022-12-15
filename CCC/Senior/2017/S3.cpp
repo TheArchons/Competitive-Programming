f77996a0e0baaf23691414221bdc8f765334b367
TheArchons <progamer9138@gmail.com>
2022-12-14 19:47:24 -0800

    ccc 2017 S3

--- CCC/Senior/2017/S3.cpp
+++ CCC/Senior/2017/S3.cpp
@@ -2,17 +2,19 @@
 
 using namespace std;
 
-// takes a vector<int> and target, and returns the number of combinations of the vector that add up to the target
-int twoSum(vector<int> nums, int target) {
+int boards[4001];
+
+// takes a target, and returns the number of combinations of the map that add up to the target
+int twoSum(int target) {
     int count = 0;
 
-    int diffs[4001] = {0};
-    for (int i = 0; i < nums.size(); i++) {
-        if (diffs[nums[i]] > 0) {
-            count++;
-            diffs[nums[i]]--;
-        } else {
-            diffs[target - nums[i]]++;
+    for (int i = 1; i <= target/2; i++) {
+        if (boards[i] > 0 && boards[target - i] > 0) {
+            if (i == target - i) {
+                count += boards[i] / 2;
+            } else {
+                count += min(boards[i], boards[target - i]);
+            }
         }
     }
 
@@ -21,20 +23,22 @@ int twoSum(vector<int> nums, int target) {
 
 int main() {
     //freopen("3.input", "r", stdin); // for testing
-    
+
     int n;
     scanf("%d", &n);
 
-    vector<int> boards(n);
     for (int i = 0; i < n; i++) {
-        scanf("%d", &boards[i]);
+        int temp;
+        scanf("%d", &temp);
+
+        boards[temp]++;
     }
 
     int maxLen, maxLenCount;
     maxLen = maxLenCount = 0;
 
     for (int i = 0; i < 4001; i++) {
-        int count = twoSum(boards, i);
+        int count = twoSum(i);
         if (count > maxLen) {
             maxLen = count;
             maxLenCount = 1;
