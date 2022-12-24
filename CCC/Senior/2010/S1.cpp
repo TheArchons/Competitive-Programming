dfb0b2d2b118bb1098946cc6cdfcad4e09f0de0e
TheArchons <alexli9138@gmail.com>
2022-12-23 16:22:36 -0800

    ccc 2010 S1

--- CCC/Senior/2010/S1.cpp
+++ CCC/Senior/2010/S1.cpp
@@ -1,32 +1,48 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-    int computerNum;
-    cin >> computerNum;
+struct computer {
+    bool operator<(const computer& other) const {
+        int score = 2*RAM + 3*CPU + HDD;
+        int otherScore = 2*other.RAM + 3*other.CPU + other.HDD;
 
-    int first, second;
-    first = second = 0;
-    string firstName, secondName;
-
-    for (int i = 0; i < computerNum; i++) {
-        string name;
-        int ram, cpu, disk;
-        cin >> name >> ram >> cpu >> disk;
-
-        int score = ram * 2 + cpu * 3 + disk;
-        if (score > first) {
-            secondName = firstName;
-            firstName = name;
-            second = first;
-            first = score;
-        } else if (score > second) {
-            secondName = name;
-            second = score;
-        }
+        if (score == otherScore) return name > other.name;
+        return score < otherScore;
     }
 
-    cout << firstName << endl << secondName << endl;
+    string name;
+    int RAM, CPU, HDD;
+};
+
+int main() {
+    cin.sync_with_stdio(0); cin.tie(0);
+    //freopen("1.input", "r", stdin); // for testing
+    
+    int computers;
+    cin >> computers;
+
+    if (computers == 0) return 0;
+
+    priority_queue<computer> computerQ;
+
+    for (int i = 0; i < computers; i++) {
+        string name;
+        int RAM, CPU, HDD;
+        cin >> name >> RAM >> CPU >> HDD;
+
+        computerQ.push({name, RAM, CPU, HDD});
+    }
+
+    // print top
+    computer top = computerQ.top();
+    cout << top.name << endl;
+    computerQ.pop();
+
+    // print second top
+    if (computerQ.empty()) return 0;
+    top = computerQ.top();
+    cout << top.name << endl;
+
+    return 0;
 }
