0275a7816d71627129a9ed6ec257e2b8bfce3db9
TheArchons <alexli9138@gmail.com>
2022-12-27 12:54:48 -0800

    CCC 2019 S3 partial

--- CCC/Senior/2019/S3.cpp
+++ CCC/Senior/2019/S3.cpp
@@ -2,40 +2,60 @@
 
 using namespace std;
 
-const int unknown = 1000001;
+#define squareType vector<vector<int>>
+#define noNumber 1000001
 
-vector<vector<int>> calcGuarantees(vector<vector<int>> square) {
-    // fill in guaranteed values
-    
-    // rows
+bool squareCompleted(squareType square) {
     for (int i = 0; i < 3; i++) {
-        int one, two, three;
-        one = square[i][0];
-        two = square[i][1];
-        three = square[i][2];
-
-        if (one != unknown && two != unknown && three == unknown) {
-            square[i][2] = two + (two - one);
-        } else if (one != unknown && two == unknown && three != unknown) {
-            square[i][1] = one + (three - one) / 2;
-        } else if (one == unknown && two != unknown && three != unknown) {
-            square[i][0] = two - (three - two);
+        for (int j = 0; j < 3; j++) {
+            if (square[i][j] == noNumber) {
+                return false;
+            }
         }
     }
 
-    // columns
-    for (int i = 0; i < 3; i++) {
-        int one, two, three;
-        one = square[0][i];
-        two = square[1][i];
-        three = square[2][i];
+    return true;
+}
 
-        if (one != unknown && two != unknown && three == unknown) {
-            square[2][i] = two + (two - one);
-        } else if (one != unknown && two == unknown && three != unknown) {
-            square[1][i] = one + (three - one) / 2;
-        } else if (one == unknown && two != unknown && three != unknown) {
-            square[0][i] = two - (three - two);
+void printSquare(squareType square) {
+    for (auto row : square) {
+        for (auto col : row) {
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+}
+
+vector<int> calculateRowColumn(vector<int> line) {
+    if (line[0] != noNumber && line[1] != noNumber && line[2] == noNumber) {
+        line[2] = line[1] + (line[1] - line[0]);
+    } else if (line[0] != noNumber && line[1] == noNumber && line[2] != noNumber) {
+        line[1] = line[0] + (line[2] - line[0]) / 2;
+    } else if (line[0] == noNumber && line[1] != noNumber && line[2] != noNumber) {
+        line[0] = line[1] - (line[2] - line[1]);
+    }
+
+    return line;
+}
+
+squareType calculateGuarantees(squareType square) {
+    //check rows
+    
+    for (int i = 0; i < 3; i++) {
+        square[i] = calculateRowColumn(square[i]);
+    }
+
+    // check columns
+    for (int i = 0; i < 3; i++) {
+        vector<int> column(3);
+        for (int j = 0; j < 3; j++) {
+            column[j] = square[j][i];
+        }
+
+        column = calculateRowColumn(column);
+
+        for (int j = 0; j < 3; j++) {
+            square[j][i] = column[j];
         }
     }
 
@@ -46,58 +66,21 @@ int main() {
     cin.sync_with_stdio(0); cin.tie(0);
     //freopen("3.input", "r", stdin); // for testing
     
-    vector<vector<int>> square(3, vector<int>(3, unknown));
+    squareType square(3, vector<int>(3, noNumber));
 
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            string temp;
-            cin >> temp;
-            if (temp != "X") {
-                square[i][j] = stoi(temp);
+            string num;
+            cin >> num;
+            if (num != "X") {
+                square[i][j] = stoi(num);
             }
         }
     }
 
-    square = calcGuarantees(calcGuarantees(square));
+    square = calculateGuarantees(calculateGuarantees(square));
 
-    // if the middle is known, then if each adjacent value is unknown, set it to the middle
-    if (square[1][1] != unknown) {
-        if (square[0][1] == unknown) {
-            square[0][1] = square[1][1];
-        }
-        if (square[1][0] == unknown) {
-            square[1][0] = square[1][1];
-        }
-        if (square[1][2] == unknown) {
-            square[1][2] = square[1][1];
-        }
-        if (square[2][1] == unknown) {
-            square[2][1] = square[1][1];
-        }
-    } else { // else, set the corners to their opposite corners if empty
-        if (square[0][0] == unknown) {
-            square[0][0] = square[2][2];
-        }
-        if (square[0][2] == unknown) {
-            square[0][2] = square[2][0];
-        }
-        if (square[2][0] == unknown) {
-            square[2][0] = square[0][2];
-        }
-        if (square[2][2] == unknown) {
-            square[2][2] = square[0][0];
-        }
-    }
-
-    square = calcGuarantees(calcGuarantees(square));
-
-    // print square
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << square[i][j] << " ";
-        }
-        cout << endl;
-    }
+    printSquare(square);
 
     return 0;
 }
