2f7520032124ddfa2bcc0ba7e6efa465e0f7709e
TheArchons <alexli9138@gmail.com>
2022-12-29 14:49:47 -0800

    usaco Dec Bronze 3 partial

--- USACO/2022/December/Bronze/3.cpp
+++ USACO/2022/December/Bronze/3.cpp
@@ -2,129 +2,109 @@
 
 using namespace std;
 
-/*
-1. for every line, compare with every other line with a different result
-2. if the same value at the same index is the same, then it's a lie
-*/
+void lie(bool isLie) {
+    if (isLie) {
+        cout << "LIE" << endl;
+    } else {
+        cout << "OK" << endl;
+    }
+    return;
+}
 
-void testCase() {
-    int valsPerLine, lineNum;
-    cin >> valsPerLine >> lineNum;
+void solve() {
+    int arrLen, arrs;
+    cin >> arrLen >> arrs;
 
-    vector<vector<bool>> lines;
-    vector<bool> results;
+    map<vector<bool>, bool> arrsMap;
+    for (int i = 0; i < arrs; i++) {
+        vector<bool> arr;
+        string s;
+        cin >> s;
 
-    for (int i = 0; i < lineNum; i++) {
-        vector<bool> line;
-        string temp;
-        cin >> temp;
-        
-        for (int j = 0; j < valsPerLine; j++) {
-            line.push_back(temp[j] == '1');
+        for (char c : s) {
+            arr.push_back(c == '1');
         }
 
         bool res;
         cin >> res;
 
-        lines.push_back(line);
-        results.push_back(res);
-    }
-
-    // check for duplicates
-    map<int, bool> seen;
-    for (int i = 0; i < lineNum; i++) {
-        int hash = 0;
-        for (int j = 0; j < valsPerLine; j++) {
-            hash += lines[i][j] * pow(10, j);
-        }
-
-        if (seen.find(hash) != seen.end()) {
-            if (seen[hash] != results[i]) {
-                cout << "LIE" << endl;
-                return;
-            }
+        if (arrsMap.find(arr) != arrsMap.end() && arrsMap[arr] != res) {
+            lie(true);
+            return;
         } else {
-            seen[hash] = results[i];
+            arrsMap[arr] = res;
         }
     }
 
-    // get the value for 00. By default, it's 0
-    int zeroRes = 0;
-    for (int i = 0; i < lineNum; i++) {
-        int oneCount = 0;
-        for (int j = 0; j < valsPerLine; j++) {
-            if (lines[i][j]) {
-                oneCount++;
+    while (true) {
+        bool split = false;
+
+        for (int i = 0; i < arrLen; i++) {
+            // check if all 1s correspond to the same result for the current bit
+            bool canSplit = true;
+            bool res;
+            for (auto it : arrsMap) {
+                if (it.first[i]) {
+                    res = it.second;
+                    break;
+                }
             }
-        }
 
-        if (oneCount == 0) {
-            zeroRes = results[i];
-        }
-    }
-    
-    // check if all 11 are valid
-    for (int i = 0; i < lineNum; i++) {
-        int oneCount = 0;
-        for (int j = 0; j < valsPerLine; j++) {
-            if (lines[i][j]) {
-                oneCount++;
+            for (auto it : arrsMap) {
+                if (it.first[i] && it.second != res) {
+                    canSplit = false;
+                    break;
+                }
             }
-        }
 
-        if (oneCount == 2) {
-            bool foundErr = false;
-
-            set<int> seen;
-
-            for (int j = 0; j < lineNum; j++) {
-                // add to seen
-                int hash = 0;
-                for (int k = 0; k < valsPerLine; k++) {
-                    if (lines[j][k]) {
-                        hash += pow(10, k);
-                    }
-                }
-
-                if (seen.find(hash) != seen.end()) {
-                    continue;
-                }
-
-                seen.insert(hash);
-                int oneCount = 0;
-                for (int k = 0; k < valsPerLine; k++) {
-                    if (lines[j][k]) {
-                        oneCount++;
-                    }
-                }
-
-                if (oneCount == 1) {
-                    if (results[i] != results[j]) {
-                        if (zeroRes == results[i] || foundErr) {
-                            cout << "LIE" << endl;
-                            return;
-                        }
-                        foundErr = true;
+            if (canSplit) {
+                // remove all lines with arr[i] == 1
+                for (auto it = arrsMap.begin(); it != arrsMap.end(); ) {
+                    if (it->first[i]) {
+                        it = arrsMap.erase(it);
+                    } else {
+                        it++;
                     }
                 }
             }
+            
+            // check if all 0s correspond to the same result for the current bit
+            canSplit = true;
+            for (auto it : arrsMap) {
+                if (!it.first[i]) {
+                    res = it.second;
+                    break;
+                }
+            }
+        }
+
+        if (!split || arrsMap.size() <= 1) {
+            break;
         }
     }
 
-    cout << "OK" << endl;
+    // if not all lines with different results were removed, then its a lie
 
+    bool res = arrsMap.begin()->second;
+    for (auto it : arrsMap) {
+        if (it.second != res) {
+            lie(true);
+            return;
+        }
+    }
+
+    lie(false);
     return;
 }
 
 int main() {
-    //freopen("re.input", "r", stdin); // for testing
-    cin.sync_with_stdio(0);
-    cin.tie(0);
+    cin.sync_with_stdio(0); cin.tie(0);
+    //freopen("3.input", "r", stdin); // for testing
     
     int testCases;
     cin >> testCases;
 
-    for (int i = 0; i < testCases; i++) {
-        testCase();
+    while (testCases--) {
+        solve();
     }
 }
