e9dc329c141121cb464f55ab2610018bbb54a98a
TheArchons <progamer9138@gmail.com>
2023-01-01 17:43:42 -0800

    ccc 2021 s3 partial

--- CCC/Senior/2021/s3.cpp
+++ CCC/Senior/2021/s3.cpp
@@ -3,43 +3,58 @@
 using namespace std;
 
 struct Friend {
-    int position;
-    int walkSpeed;
-    int hearingDistance;
-};
-
-bool operator<(const Friend& a, const Friend& b) {
-    return a.position < b.position;
-}
+    int position, walkSpeed, hearDist;
+}p;
 
 int main() {
     cin.sync_with_stdio(0); cin.tie(0);
-    freopen("3.input", "r", stdin); // for testing
+    //freopen("3.input", "r", stdin); // for testing
     
-    int numFriends;
-    cin >> numFriends;
+    int friendNum;
+    cin >> friendNum;
 
-    set<Friend> friends;
-    for (int i = 0; i < numFriends; i++) {
-        int position, walkSpeed, hearingDistance;
-        cin >> position >> walkSpeed >> hearingDistance;
-        friends.insert({position, walkSpeed, hearingDistance});
+    vector<int> diffs(1000000);
+    int minFriend, maxFriend;
+    minFriend = INT_MAX;
+    maxFriend = 0;
+    vector<Friend> friends(friendNum);
+
+    for (int i = 0; i < friendNum; i++) {
+        int p, w, d;
+        cin >> p >> w >> d;
+
+        friends[i] = {p, w, d};
     }
 
-    int minTime = INT_MAX;
-    for (int i = friends.begin()->position; i <= friends.rbegin()->position; i++) {
-        int currTime = 0;
-        for (auto it : friends) {
-            int dist = abs(it.position - i);
-            if (dist > it.hearingDistance) {
-                currTime += (dist - it.hearingDistance) * it.walkSpeed;
-            }
+    for (auto [p, w, d] : friends) {
+        diffs[max(p-d, 0)] += w;
+        diffs[p+d] += w;
+
+        minFriend = min(minFriend, max(p-d, 0));
+        maxFriend = max(maxFriend, p);
+    }
+
+    int walkDist = 0;
+    int currDiff = 0;
+
+    for (auto [p, w, d] : friends) {
+        currDiff -= w;
+        if (p - d > minFriend) {
+            walkDist += (p - d - minFriend) * w;
         }
-
-        minTime = min(minTime, currTime);
     }
 
-    cout << minTime << endl;
+    currDiff += diffs[minFriend];
+
+    int minWalkDist = walkDist;
+
+    for (int i = minFriend+1; i <= maxFriend; i++) {
+        walkDist += currDiff;
+        minWalkDist = min(minWalkDist, walkDist);
+        currDiff += diffs[i];
+    }
+
+    cout << minWalkDist << endl;
 
     return 0;
 }
