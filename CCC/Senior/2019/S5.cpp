c0f2f21638f684fc275abbe24a97965c60784796
TheArchons <progamer9138@gmail.com>
2023-02-03 16:13:02 -0800

    CCC 2019 S5 partial

--- CCC/Senior/2019/S5.cpp
+++ CCC/Senior/2019/S5.cpp
@@ -2,33 +2,41 @@
 
 using namespace std;
 
+#define ll long long
+
 int main() {
     cin.sync_with_stdio(0); cin.tie(0);
     //freopen("5.input", "r", stdin); // for testing. Comment out for submissions
 
-    int triangleHeight; cin >> triangleHeight;
-    int subTriangleHeight; cin >> subTriangleHeight;
+    int height; cin >> height;
+    int subHeight; cin >> subHeight;
 
-    vector<vector<int>> triangle(triangleHeight);
-
-    for (int i = 0; i < triangleHeight; i++) {
-        triangle[i].resize(i + 1);
+    vector<vector<int>> triangle;
+    for (int i = 0; i < height; i++) {
+        vector<int> row;
         for (int j = 0; j < i + 1; j++) {
-            cin >> triangle[i][j];
+            int temp; cin >> temp;
+
+            row.push_back(temp);
         }
+        triangle.push_back(row);
     }
 
-    int total = 0;
+    ll total = 0;
 
-    for (int i = 0; i < triangleHeight - subTriangleHeight + 1; i++) {
-        for (int j = 0; j < triangle[i].size(); j++) {
-            int maxSize = 0;
-            for (int k = 0; k < subTriangleHeight; k++) {
+    for (int i = 0; i < height - subHeight + 1; i++) {
+        for (int j = 0; j < i + 1; j++) {
+            ll max = 0;
+            for (int k = 0; k < subHeight; k++) {
                 for (int l = 0; l < k + 1; l++) {
-                    maxSize = max(maxSize, triangle[i + k][j + l]);
+                    ll current = triangle[i + k][j + l];
+                    if (current > max) {
+                        max = current;
+                    }
                 }
             }
-            total += maxSize;
+
+            total += max;
         }
     }
 
