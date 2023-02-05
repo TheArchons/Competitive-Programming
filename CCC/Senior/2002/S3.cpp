e5eb0ae38010a823c733c11c98e1349ca7a80d35
TheArchons <progamer9138@gmail.com>
2023-02-05 12:23:30 -0800

    CCC 2002 S3

--- CCC/Senior/2002/S3.cpp
+++ CCC/Senior/2002/S3.cpp
@@ -2,64 +2,44 @@
 
 using namespace std;
 
+set<pair<int, int>> checkPositions;
+int rowNum, columnNum;
+int finalAddRow, finalAddColumn;
+
 vector<vector<int>> grid;
 
-vector<char> directions;
-
-int directionNum, rowNum, columnNum;
-int rightDiff = 0;
-int forwardDiff = 0;
-
-// 0 = up, 1 = right, 2 = down, 3 = left
 void traverse(int row, int column, int direction) {
-    int endRow, endColumn;
-    endRow = row; endColumn = column;
-    if (direction == 0) {
-        endRow -= forwardDiff;
-        endColumn += rightDiff;
-    } else if (direction == 1) {
-        endRow += rightDiff;
-        endColumn += forwardDiff;
-    } else if (direction == 2) {
-        endRow += forwardDiff;
-        endColumn -= rightDiff;
-    } else {
-        endRow -= rightDiff;
-        endColumn -= forwardDiff;
-    }
+    for (auto it = checkPositions.begin(); it != checkPositions.end(); it++) {
+        int currRow, currColumn;
+        
+        if (direction == 0) {
+            currRow = row - it->first;
+            currColumn = column + it->second;
+        } else if (direction == 1) {
+            currRow = row + it->second;
+            currColumn = column + it->first;
+        } else if (direction == 2) {
+            currRow = row + it->first;
+            currColumn = column - it->second;
+        } else if (direction == 3) {
+            currRow = row - it->second;
+            currColumn = column - it->first;
+        }
 
-    if (endRow < 0 || endRow >= rowNum || endColumn < 0 || endColumn >= columnNum || !grid[endRow][endColumn]) {
-        return;
-    }
-
-    for (int i = 0; i < directionNum; i++) {
-        char currDirection = directions[i];
-
-        if (currDirection == 'L') {
-            direction -= 1;
-            if (direction < 0) direction = 3;
-        } else if (currDirection == 'R') {
-            direction += 1;
-            if (direction > 3) direction = 0;
-        } else {
-            if (direction == 0) {
-                row -= 1;
-            } else if (direction == 1) {
-                column += 1;
-            } else if (direction == 2) {
-                row += 1;
-            } else {
-                column -= 1;
-            }
-
-            // if out of bounds or blocked
-            if (row < 0 || row >= rowNum || column < 0 || column >= columnNum || !grid[row][column]) {
-                return;
-            }
+        if (currRow < 0 || currRow >= rowNum || currColumn < 0 || currColumn >= columnNum || !grid[currRow][currColumn]) {
+            return;
         }
     }
 
-    grid[row][column] = 2;
+    if (direction == 0) {
+        grid[row - finalAddRow][column + finalAddColumn] = 2;
+    } else if (direction == 1) {
+        grid[row + finalAddColumn][column + finalAddRow] = 2;
+    } else if (direction == 2) {
+        grid[row + finalAddRow][column - finalAddColumn] = 2;
+    } else if (direction == 3) {
+        grid[row - finalAddColumn][column - finalAddRow] = 2;
+    }
 
     return;
 }
@@ -68,9 +48,11 @@ int main() {
     cin.sync_with_stdio(0); cin.tie(0);
     //freopen("3.input", "r", stdin); // for testing. Comment out for submissions
 
-    rowNum; cin >> rowNum; columnNum; cin >> columnNum;
+    cin >> rowNum;
 
-    grid.resize(rowNum, vector<int>(columnNum)); // 0 = blocked, 1 = open, 2 = possible end position
+    cin >> columnNum;
+
+    grid.resize(rowNum, vector<int>(columnNum)); // 0 is blocked, 1 is open, 2 is a final position
 
     for (int i = 0; i < rowNum; i++) {
         for (int j = 0; j < columnNum; j++) {
@@ -80,62 +62,58 @@ int main() {
         }
     }
 
-    directionNum; cin >> directionNum;
+    checkPositions.insert({0, 0});
 
-    directions.reserve(directionNum);
+    int currentRow, currentColumn, currDirection; // starting at 0, 0, facing north
+    currentRow = currentColumn = currDirection = 0;
 
-    for (int i = 0; i < directionNum; i++) {
-        cin >> directions[i];
+    int moveNum; cin >> moveNum;
+
+    for (int i = 0; i < moveNum; i++) {
+        char currMove; cin >> currMove;
+
+        if (currMove == 'F') {
+            if (currDirection == 0) {
+                currentRow++;
+            } else if (currDirection == 1) {
+                currentColumn++;
+            } else if (currDirection == 2) {
+                currentRow--;
+            } else if (currDirection == 3) {
+                currentColumn--;
+            }
+        } else if (currMove == 'R') {
+            currDirection = (currDirection + 1) % 4;
+        } else if (currMove == 'L') {
+            currDirection = (currDirection + 3) % 4;
+        }
+
+        checkPositions.insert({currentRow, currentColumn});
     }
 
-    // calculateDiff
-    int currFacing = 0;
+    finalAddColumn = currentColumn;
+    finalAddRow = currentRow;
 
-    for (int i = 0; i < directionNum; i++) {
-        char currDirection = directions[i];
-
-        if (currDirection == 'L') {
-            currFacing -= 1;
-            if (currFacing < 0) currFacing = 3;
-        } else if (currDirection == 'R') {
-            currFacing += 1;
-            if (currFacing > 3) currFacing = 0;
-        } else {
-            if (currFacing == 0) {
-                forwardDiff += 1;
-            } else if (currFacing == 1) {
-                rightDiff += 1;
-            } else if (currFacing == 2) {
-                forwardDiff -= 1;
-            } else {
-                rightDiff -= 1;
+    for (int i = 0; i < rowNum; i++) {
+        for (int j = 0; j < columnNum; j++) {
+            for (int k = 0; k < 4; k++) {
+                traverse(i, j, k);
             }
         }
     }
 
     for (int i = 0; i < rowNum; i++) {
         for (int j = 0; j < columnNum; j++) {
-            if (!grid[i][j]) continue;
-
-            traverse(i, j, 0);
-            traverse(i, j, 1);
-            traverse(i, j, 2);
-            traverse(i, j, 3);
-        }
-    }
-
-    for (int i = 0; i < rowNum; i++) {
-        for (int j = 0; j < columnNum; j++) {
-            int curr = grid[i][j];
-            if (curr == 0) {
+            if (!grid[i][j]) {
                 cout << 'X';
-            } else if (curr == 1) {
+            } else if (grid[i][j] == 1) {
                 cout << '.';
             } else {
                 cout << '*';
             }
         }
-        cout << '\n';
+
+        cout << endl;
     }
 
     return 0;
