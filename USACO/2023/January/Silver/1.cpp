04140c1b4c10a3c79488df8a4fd68f51033dc4bd
TheArchons <progamer9138@gmail.com>
2023-02-07 11:59:43 -0800

    USACO 2023 January Silver 1 partial

--- USACO/2023/January/Silver/1.cpp
+++ USACO/2023/January/Silver/1.cpp
@@ -2,53 +2,113 @@
 
 using namespace std;
 
-int main() {
-    cin.sync_with_stdio(0); cin.tie(0);
-    //freopen("1.input", "r", stdin); // for testing
+int charToInt(char c) {
+    if (c >= 'a') {
+        return c - 'a' + 26;
+    } else {
+        return c - 'A';
+    }
+}
 
-    int cases;
-    cin >> cases;
-    nextCase:while (cases--) {
-        string start, end;
-        cin >> start >> end;
+void solve() {
+    string startStr; cin >> startStr;
+    string endStr; cin >> endStr;
 
-        int sLen, eLen;
-        sLen = start.length();
-        eLen = end.length();
-        int extra = 0;
-        if (sLen != eLen) {
+    int strLen = startStr.length();
+
+    vector<char> graph(52, '.');
+
+    int total = 0;
+
+    for (int i = 0; i < strLen; i++) {
+        char curr = startStr[i];
+        int currInt = charToInt(curr);
+
+        char endCurr = endStr[i];
+
+        char currGraph = graph[currInt];
+
+        if (currGraph == '.') {
+            graph[currInt] = endCurr;
+            if (curr != endCurr) {
+                total++;
+            }
+        } else if (currGraph != endCurr) {
             cout << -1 << endl;
-            goto nextCase;
+            return;
         }
+    }
 
-        if (start == end) {
-            cout << 0 << endl;
-            goto nextCase;
-        }
+    bool usedAll = false;
 
-        map<char, char> sToE;
-        map<char, char> seenExtras;
-        for (int i = 0; i < sLen; i++) {
-            if (start[i] != end[i]) {
-                if (sToE.find(start[i]) == sToE.end() || sToE[start[i]] == end[i]) {
-                    sToE[start[i]] = end[i];
-                } else {
-                    cout << -1 << endl;
-                    goto nextCase;
-                }
+    if (startStr != endStr) {
+        vector<bool> visited(52, false);
+        int uniqueCount = 0;
 
-                if (sToE.find(end[i]) != sToE.end() && sToE[end[i]] == start[i] && seenExtras.find(start[i]) == seenExtras.end()) {
-                    extra++;
-                    seenExtras[start[i]] = end[i];
-                    seenExtras[end[i]] = start[i];
-                }
-            } else if (sToE.find(start[i]) != sToE.end()) {
-                cout << -1 << endl;
-                goto nextCase;
+        for (int i = 0; i < strLen; i++) {
+            if (!visited[i]) {
+                uniqueCount++;
+                visited[i] = true;
             }
         }
 
-        cout << sToE.size() + extra << endl;
+        usedAll = uniqueCount >= 52;
+    }
+
+    // if usedAll and has loop, impossible
+
+    // loop detection
+    bool foundLoop = false;
+    for (int i = 0; i < 52; i++) {
+        vector<bool> visitedNodes(52, false);
+        queue<char> q;
+        // add to queue
+        if (i < 26) {
+            q.push('A' + i);
+        } else {
+            q.push('a' + i - 26);
+        }
+
+        while (!q.empty()) {
+            char curr = q.front();
+            q.pop();
+
+            int currInt = charToInt(curr);
+            char next = graph[currInt];
+
+            if (visitedNodes[currInt] && next != curr) {
+                foundLoop = true;
+                total++;
+                graph[currInt] = '.'; // remove loop
+                break;
+            }
+
+            visitedNodes[currInt] = true;
+
+            if (next != '.' && next != curr) {
+                q.push(next);
+            }
+        }
+    }
+
+    if (usedAll && foundLoop) {
+        cout << -1 << endl;
+        return;
+    }
+
+    cout << total << endl;
+
+    return;
+}
+
+int main() {
+    cin.sync_with_stdio(0); cin.tie(0);
+    //freopen("1.input", "r", stdin); // for testing. Comment out for submissions
+
+    int testCases; cin >> testCases;
+
+    while (testCases--) {
+        solve();
     }
 
     return 0;
