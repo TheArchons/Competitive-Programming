8bace0b5e638efc215a1a72d4fc9809f25acbc15
TheArchons <progamer9138@gmail.com>
2023-02-10 18:01:37 -0800

    AtCoder Educational DP Contest f

--- AtCoder/dp/f.cpp
+++ AtCoder/dp/f.cpp
@@ -2,64 +2,57 @@
 
 using namespace std;
 
-bool isEnd = false;
-
-vector<vector<int>> dp;
-
-string firstStr, secondStr;
-int firstLen, secondLen;
-
-void dfs(int i, int j, vector<char> &currStr) {
-    if (isEnd) return;
-
-    if (i == firstLen && j == secondLen && dp[i][j] == currStr.size()) {
-        for (char c : currStr) {
-            cout << c;
-        }
-        cout << endl;
-        isEnd = true;
-        return;
-    }
-
-    if (firstStr[i] == secondStr[j]) {
-        currStr.push_back(firstStr[i]);
-        dfs(i + 1, j + 1, currStr);
-        currStr.pop_back();
-    }
-
-    if (i + 1 <= firstLen) {
-        dfs(i + 1, j, currStr);
-    }
-
-    if (j + 1 <= secondLen) {
-        dfs(i, j + 1, currStr);
-    }
-
-    return;
-}
-
 int main() {
     cin.sync_with_stdio(0); cin.tie(0);
-    //freopen("f.input", "r", stdin); // for testing
+    //freopen("f.input", "r", stdin); // for testing. Comment out for submissions
 
-    cin >> firstStr >> secondStr;
+    string string1; cin >> string1;
+    string string2; cin >> string2;
 
-    firstLen = firstStr.length();
-    secondLen = secondStr.length();
+    int string1Len = string1.length();
+    int string2Len = string2.length();
 
-    dp.resize(firstLen + 1, vector<int>(secondLen + 1, 0));
-    for (int i = 1; i <= firstLen; i++) {
-        for (int j = 1; j <= secondLen; j++) {
-            if (firstStr[i - 1] == secondStr[j - 1]) {
+    vector<vector<int>> dp(string1Len + 1, vector<int>(string2Len + 1, 0));
+
+    vector<vector<pair<int, int>>> path(string1Len + 1, vector<pair<int, int>>(string2Len + 1, {0, 0}));
+    
+    for (int i = 1; i <= string1Len; i++) {
+        for (int j = 1; j <= string2Len; j++) {
+            char char1 = string1[i - 1];
+            char char2 = string2[j - 1];
+
+            if (char1 == char2) {
                 dp[i][j] = dp[i - 1][j - 1] + 1;
+                path[i][j] = {i - 1, j - 1};
             } else {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                if (dp[i - 1][j] > dp[i][j - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                    path[i][j] = {i - 1, j};
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                    path[i][j] = {i, j - 1};
+                }
             }
         }
     }
 
-    vector<char> currStr;
-    dfs(0, 0, currStr);
+    string lcs = "";
+
+    int x, y;
+    x = string1Len;
+    y = string2Len;
+
+    while (x != 0 && y != 0) {
+        if (path[x][y] == make_pair(x - 1, y - 1)) {
+            lcs += string1[x - 1];
+        }
+
+        tie(x, y) = path[x][y];
+    }
+
+    for (int i = lcs.length() - 1; i >= 0; i--) {
+        cout << lcs[i];
+    }
 
     return 0;
 }
