3036756c2102f90cb62ae196c494a329947dae4e
TheArchons <progamer9138@gmail.com>
2023-02-10 20:10:10 -0800

    AtCoder Educational DP Contest g

--- AtCoder/dp/g.cpp
+++ AtCoder/dp/g.cpp
@@ -2,68 +2,42 @@
 
 using namespace std;
 
-int main() {
-    cin.sync_with_stdio(0); cin.tie(0);
-    //freopen("g.input", "r", stdin); // for testing. Comment out for submissions
+vector<vector<int>> grid;
+vector<int> dp;
 
-    int nodes; cin >> nodes;
-    int edges; cin >> edges;
-
-    vector<vector<int>> connections(nodes + 1);
-
-    for (int i = 0; i < edges; i++) {
-        int start; cin >> start;
-        int end; cin >> end;
-
-        connections[start].push_back(end);
-    }
-
-    set<int> notVisited;
-    for (int i = 1; i <= nodes; i++) {
-        notVisited.insert(i);
-    }
-
-    for (int i = 1; i <= nodes; i++) {
-        queue<int> q;
-        q.push(i);
-
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-
-            for (auto connection : connections[node]) {
-                if (notVisited.find(connection) != notVisited.end()) {
-                    notVisited.erase(connection);
-                    q.push(connection);
-                }
-            }
-        }
-    }
+int solve(int curr) {
+    if (dp[curr] != -1) return dp[curr];
 
     int maxPath = 0;
 
-    for (auto root : notVisited) {
-        vector<vector<int>> longestPath(nodes + 1, vector<int>(nodes + 1, 0));
-
-        priority_queue<pair<int, int>> pq;
-        pq.push({root, 0});
-        
-        while (!pq.empty()) {
-            auto [node, pathLength] = pq.top();
-            pq.pop();
-
-            maxPath = max(maxPath, pathLength);
-
-            for (auto connection : connections[node]) {
-                if (pathLength + 1 > longestPath[connection][node]) {
-                    longestPath[connection][node] = pathLength + 1;
-                    pq.push({connection, pathLength + 1});
-                }
-            }
-        }
+    for (int i = 0; i < grid[curr].size(); i++) {
+        maxPath = max(maxPath, solve(grid[curr][i]) + 1);
     }
 
+    return dp[curr] = maxPath;
+}
 
+int main() {
+    cin.sync_with_stdio(0); cin.tie(0);
+    //freopen("g.input", "r", stdin); // For testing. Comment out for submissions
+
+    int vertexNum; cin >> vertexNum;
+    int edgeNum; cin >> edgeNum;
+
+    grid.resize(vertexNum + 1);
+
+    for (int i = 0; i < edgeNum; i++) {
+        int a, b; cin >> a >> b;
+        grid[a].push_back(b);
+    }
+
+    dp.resize(vertexNum + 1, -1);
+
+    int maxPath = 0;
+
+    for (int i = 1; i <= vertexNum; i++) {
+        maxPath = max(maxPath, solve(i));
+    }
 
     cout << maxPath << endl;
 
