572e8e095d8a0a533672aca00bdde48c4dbc84c2
TheArchons <progamer9138@gmail.com>
2023-02-12 22:04:57 -0800

    CCC 2022 S3 partial
    segfaultttttttttttttttttttttttttttttttttttttttttttttttt

--- CCC/Senior/2022/S3.cpp
+++ CCC/Senior/2022/S3.cpp
@@ -2,65 +2,61 @@
 
 using namespace std;
 
+int noteNum;
+int maxPitch;
+int goodSamples;
+
+vector<int> notes;
+
 int main() {
     cin.sync_with_stdio(0); cin.tie(0);
-    //freopen("3.input", "r", stdin); // for testing
-    
-    int notes, maxPitch, sampleNum;
-    cin >> notes >> maxPitch >> sampleNum;
+    //freopen("3.input", "r", stdin); // For testing. Comment out for submissions
 
-    if (notes > sampleNum) {
-        cout << "-1" << endl;
+    cin >> noteNum;
+    cin >> maxPitch;
+    cin >> goodSamples;
+
+    notes.resize(noteNum, -1); // the first maxPitch notes are just for padding. Start at + maxPitch
+
+    int currSamples = noteNum;
+
+    if (goodSamples < currSamples) {
+        cout << "-1\n";
         return 0;
     }
 
-    vector<int> pitches(notes, 1);
-    int currSamples = notes;
-    int regularMax = maxPitch - 1;
-    int lastPitchLocation = 1;
+    int currNote;
 
-    for (int i = 1; i < notes; i++) {
-        int pitch;
-        int wantedSamples = sampleNum - currSamples;
-        if (!wantedSamples) break;
-        lastPitchLocation = i;
-        int prevPitch = pitches[i-1];
-        if (i < maxPitch) {
-            if (wantedSamples >= i) {
-                pitch = i + 1;
-                currSamples += pitch - 1;
-            } else {
-                pitch = i - wantedSamples;
-                currSamples += wantedSamples;
-            }
-        } else {
-            pitch = prevPitch >= maxPitch ? 1 : prevPitch + 1;
-            if (wantedSamples < regularMax) {
-                pitch += wantedSamples;
-                currSamples += wantedSamples;
-            } else {
-                currSamples += regularMax;
-            }
+    for (currNote = 0; currNote < maxPitch && currNote <= goodSamples - currSamples; currNote++) {
+        notes[currNote] = currNote + 1;
+        currSamples += currNote;
+    }
+
+    for (; currNote < noteNum && currSamples + maxPitch - 1 <= goodSamples; currNote++) {
+        notes[currNote] = notes[currNote - maxPitch];
+        currSamples += maxPitch - 1;
+    }
+
+    if (currNote < noteNum) {
+        notes[currNote] = notes[currNote - (goodSamples - currSamples) - 1];
+        currSamples = goodSamples;
+        currNote++;
+    }
+
+    if (currSamples != goodSamples) {
+        cout << "-1\n";
+        return 0;
+    }
+
+    int lastNote = notes[currNote - 1];
+
+    for (int i = 0; i < noteNum; i++) {
+        if (notes[i] == -1) {
+            notes[i] = lastNote;
         }
 
-        pitches[i] = pitch;
+        cout << notes[i] << " ";
     }
-
-    // set everything past lastPitchLocation to the last pitch
-    for (int i = lastPitchLocation + 1; i < notes; i++) {
-        pitches[i] = pitches[lastPitchLocation];
-    }
-
-    if (sampleNum != currSamples) {
-        cout << "-1" << endl;
-        return 0;
-    }
-
-    for (int i = 0; i < notes; i++) {
-        cout << pitches[i] << " ";
-    }
-
-    cout << endl;
 
     return 0;
 }
