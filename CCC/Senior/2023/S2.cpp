5bcf3a701b3cf14fcf1a0a33368be7d1e3c6a0e4
TheArchons <progamer9138@gmail.com>
2023-02-19 13:29:20 -0800

    CCC 2023 S2
    AAAAAAAAAAAAAAAAA IT WAS SO EASYYYY

--- CCC/Senior/2023/S2.cpp
+++ CCC/Senior/2023/S2.cpp
@@ -2,60 +2,47 @@
 
 using namespace std;
 
+vector<int> mountains;
+vector<int> mins;
+int mountainNum;
+
+
+void calcValue(int l, int r) {
+    int val = 0;
+
+    while (l >= 0 && r < mountainNum) {
+        val += abs(mountains[l] - mountains[r]);
+        mins[r - l] = min(mins[r - l], val);
+
+        l--; r++;
+    }
+
+    return;
+}
+
 int main() {
     cin.sync_with_stdio(0); cin.tie(0);
-    //freopen("2.input", "r", stdin); // For testing. Comment out for submissions
+    // freopen("2.input", "r", stdin); // For testing. Comment out for submissions
 
-    int mountainNum; cin >> mountainNum;
+    cin >> mountainNum;
 
-    vector<int> mountains(mountainNum);
-    vector<int> prefixSum(mountainNum + 1);
-
-    int total = 0;
+    mountains.resize(mountainNum);
 
     for (int i = 0; i < mountainNum; i++) {
         cin >> mountains[i];
-        total += mountains[i];
-        prefixSum[i + 1] = total;
     }
 
-    for (int i = 0; i < mountainNum; i++) { // lengths
-        int minVal = pow(10, 9);
-        for (int left = 0; left < mountainNum - i; left++) {
-            int right = left + i;
-            if (mountainNum <= 300) {
-                int tempL, tempR;
-                tempL = left;
-                tempR = right;
-                int total = 0;
+    mins.resize(mountainNum, INT_MAX);
 
-                while (tempL < tempR) {
-                    total += abs(mountains[tempR] - mountains[tempL]);
-                    tempL++;
-                    tempR--;
-                }
-
-                minVal = min(minVal, total);
-            } else {
-                if (i == 0) {
-                    minVal = 0;
-                    continue;
-                }
-                int midPref = prefixSum[left + (i + 1) / 2];
-                // sum of the left side before the middle
-                int leftSum = midPref - prefixSum[left];
-                // sum of the right side after the middle
-                int a = left + (i + 1) / 2;
-                if ((left - right) % 2 == 0) {
-                    a++;
-                }
-                int rightSum = prefixSum[right + 1] - prefixSum[a];
-
-                minVal = min(minVal, abs(leftSum - rightSum));
-            }
-
+    for (int i = 0; i < mountainNum; i++) {
+        calcValue(i, i);
+        if (i < mountainNum - 1) {
+            calcValue(i, i + 1);
         }
-        cout << minVal << " ";
+    }
+
+    for (int i = 0; i < mountainNum; i++) {
+        cout << mins[i] << " ";
     }
 
     return 0;
