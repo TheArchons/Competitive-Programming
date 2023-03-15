d3a2a403b5b252752c7e2d63cb11bc1fc91d2a7d
TheArchons <progamer9138@gmail.com>
2023-03-15 12:59:22 -0700

    USACO 2023 Feb Bronze P2 Partial

---
+++ USACO/2023/February/Bronze/2.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int gridLen; cin >> gridLen;
+
+    vector<vector<bool>> grid(gridLen, vector<bool>(gridLen)); // true = has ink, false = no ink
+
+    for (int i = 0; i < gridLen; i++) {
+        for (int j = 0; j < gridLen; j++) {
+            char c; cin >> c;
+
+            grid[i][j] = c == '*';
+        }
+    }
+
+    int stampLen; cin >> stampLen;
+
+    vector<vector<bool>> stamp(stampLen, vector<bool>(stampLen));
+
+    for (int i = 0; i < stampLen; i++) {
+        for (int j = 0; j < stampLen; j++) {
+            char c; cin >> c;
+
+            stamp[i][j] = c == '*';
+        }
+    }
+
+    vector<vector<bool>> result(gridLen, vector<bool>(gridLen, false));
+    for (int rotation = 0; rotation < 4; rotation++) {
+        for (int i = 0; i < gridLen - stampLen + 1; i++) {
+            for (int j = 0; j < gridLen - stampLen + 1; j++) {
+                for (int k = 0; k < stampLen; k++) {
+                    for (int l = 0; l < stampLen; l++) {
+                        if (grid[i + k][j + l] && stamp[k][l]) {
+                            result[i + k][j + l] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        vector<vector<bool>> newStamp(stampLen, vector<bool>(stampLen));
+
+        for (int i = 0; i < stampLen; i++) {
+            for (int j = 0; j < stampLen; j++) {
+                newStamp[i][j] = stamp[j][stampLen - 1 - i];
+            }
+        }
+
+        stamp = newStamp;
+    }
+
+    for (int i = 0; i < gridLen; i++) {
+        for (int j = 0; j < gridLen; j++) {
+            if (grid[i][j] != result[i][j]) {
+                cout << "NO" << endl;
+                return;
+            }
+        }
+    }
+
+    cout << "YES" << endl;
+}
+
+int main() {
+    cin.sync_with_stdio(0); cin.tie(0);
+    // freopen("2.input", "r", stdin); // For testing. Comment out for submissions
+
+    int testCases; cin >> testCases;
+
+    while (testCases--) {
+        solve();
+    }
+
+    return 0;
+}
