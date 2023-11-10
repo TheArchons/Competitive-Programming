cfeba8ff9c354e9f9c3da35c6f7267af065e54c0
TheArchons <progamer9138@gmail.com>
2023-11-10 12:11:05 -0800

    AC hhkdb_2022_winter e partial
    just need a fast way to compute score, using Binary Exponentiation

---
+++ AtCoder/hhkb_2022_Winter/e.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+struct ballEdge {
+    int ball1;
+    int ball2;
+    ll score;
+
+    bool operator<(const ballEdge& b) const {
+        return this->score < b.score;
+    }
+};
+
+const int maxBallNum = 505;
+
+vector<int> ds(maxBallNum);
+vector<int> dsh(maxBallNum, 1);
+
+ll totalScore = 0;
+
+int findRep(int ball) {
+    while(ds[ball] != ball) {
+        ball = ds[ball];
+    }
+
+    return ball;
+}
+
+void unionBalls(int rep1, int rep2) {
+    if (dsh[rep1] < dsh[rep2]) {
+        swap(rep1, rep2);
+    }
+
+    if (dsh[rep1] == dsh[rep2]) {
+        dsh[rep1]++;
+    }
+
+    ds[rep2] = rep1;
+
+    return;
+}
+
+void initDs() {
+    for (int i = 0; i < maxBallNum; i++) {
+        ds[i] = i;
+    }
+}
+
+int main() {
+    cin.sync_with_stdio(0); cin.tie(0);
+    freopen("e.input", "r", stdin); // For testing. Comment out for submissions
+
+    initDs();
+
+    int ballNum; cin >> ballNum;
+    ll maxInt; cin >> maxInt;
+
+    vector<int> balls(ballNum);
+
+    for (int i = 0; i < ballNum; i++) {
+        cin >> balls[i];
+    }
+
+    priority_queue<ballEdge> ballEdges;
+
+    for (int i = 0; i < ballNum; i++) {
+        ll currBall = balls[i];
+
+        for (int j = i+1; j < ballNum; j++) {
+            ll ball2 = balls[j];
+
+            ballEdge currEdge;
+            currEdge.ball1 = i;
+            currEdge.ball2 = j;
+            currEdge.score = (ll)(pow(currBall, ball2) + pow(ball2, currBall)) % maxInt;
+
+            ballEdges.push(currEdge);
+        }
+    }
+
+    while (!ballEdges.empty()) {
+        ballEdge currBall = ballEdges.top();
+        ballEdges.pop();
+
+        int rep1, rep2;
+        rep1 = findRep(currBall.ball1);
+        rep2 = findRep(currBall.ball2);
+
+        if (rep1 != rep2) {
+            totalScore += currBall.score;
+
+            unionBalls(rep1, rep2);
+        }
+    }
+
+    cout << totalScore << endl;
+
+    return 0;
+}
